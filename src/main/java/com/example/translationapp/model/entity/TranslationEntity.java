29b6c5e19bbb7c0b2c9a028f11224d233e33d714
thoanguyen2907 <thoanguyen290795@gmail.com>
2025-01-20 23:50:48 +0700

    add seeding data

--- src/main/java/com/example/translationapp/model/entity/TranslationEntity.java
+++ src/main/java/com/example/translationapp/model/entity/TranslationEntity.java
@@ -1,6 +1,7 @@
 package com.example.translationapp.model.entity;
 
 import com.example.translationapp.model.base.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
@@ -15,21 +16,25 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class TranslationEntity extends BaseEntity {
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
-    private UUID id;
 
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @JsonProperty("id")
+    private Integer id;
+
+    @JsonProperty("wordFirstLang")
     @Column(name = "word_first_lang", nullable = false)
     private String wordFirstLang;
 
+    @JsonProperty("sentenceFirstLang")
     @Column(name = "sentence_first_lang", nullable = false)
     private String sentenceFirstLang;
 
+    @JsonProperty("wordSecondLang")
     @Column(name = "word_second_lang", nullable = false)
     private String wordSecondLang;
 
+    @JsonProperty("sentenceSecondLang")
     @Column(name = "sentence_second_lang", nullable = false)
     private String sentenceSecondLang;
 }
