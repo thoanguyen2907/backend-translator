b42611924448dba4e2c63ded88c253a673730212
thoanguyen2907 <thoanguyen290795@gmail.com>
2025-01-21 10:11:09 +0700

    add edit feature

--- src/main/java/com/example/translationapp/controller/TranslatorController.java
+++ src/main/java/com/example/translationapp/controller/TranslatorController.java
@@ -2,14 +2,14 @@ package com.example.translationapp.controller;
 
 import com.example.translationapp.controller.route.CommonRoute;
 import com.example.translationapp.controller.route.TranslatorRoute;
+import com.example.translationapp.dto.TranslatorUpdateDto;
 import com.example.translationapp.service.ITranslatorService;
 import com.example.translationapp.shared.ResponseEntityBuilder;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(CommonRoute.BASE_API + CommonRoute.VERSION)
@@ -19,6 +19,7 @@ public class TranslatorController {
     public TranslatorController(ITranslatorService translatorService) {
         this.translatorService = translatorService;
     }
+
     @GetMapping(TranslatorRoute.BASE_URL)
     public ResponseEntity<?> getAllTranslators (
             @RequestParam(name = "offset", defaultValue = "0") final Integer offset,
@@ -30,5 +31,12 @@ public class TranslatorController {
                 .getBuilder()
                 .setDetails(productList)
                 .build();
+    }
+
+    @PatchMapping(TranslatorRoute.BASE_URL + "/{translatorId}")
+    public ResponseEntity<?> updateTranslator (@PathVariable final String translatorId, @RequestBody TranslatorUpdateDto translatorUpdateDto) {
+        var convertedId = Integer.parseInt(translatorId);
+        var translatorResponse =  translatorService.updateTranslator(convertedId, translatorUpdateDto);
+        return ResponseEntity.ok(translatorResponse);
     }
 }
--- src/main/java/com/example/translationapp/repository/TranslatorRepository.java
+++ src/main/java/com/example/translationapp/repository/TranslatorRepository.java
@@ -4,9 +4,14 @@ import com.example.translationapp.model.entity.TranslationEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TranslatorRepository extends JpaRepository<TranslationEntity, Integer> {
     Page<TranslationEntity> findByWordFirstLangContaining(String keyword, Pageable pageable);
+
+    @Query(value = "SELECT * FROM translates t ORDER BY t.id ASC", nativeQuery = true)
+    Page<TranslationEntity> findAllOrderedById(Pageable pageable);
+
 }
